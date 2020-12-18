a793cf59eebac179977c1314efc42a994887aa52
russellsutt <russell.sutter@gmail.com>
2020-12-18 12:38:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function findMinAndRemove(array){
 
+function findMinAndRemove(array) {
+    let min = array[0];
+    let index = 0
+    for (let i = 0; i < array.length; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+            index = i
+        }
+    }
+    array.splice(index, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    let newMin;
+    let sorted = [];
+    while (array.length !== 0) {
+        newMin = findMinAndRemove(array);
+        sorted.push(newMin);
+    }
+    return sorted
 }
